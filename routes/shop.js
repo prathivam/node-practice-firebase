6a58c52f3d24c5e73691fc65927a662300cdd795
sahrudhweb <sahrudhweb@gmail.com>
2025-11-07 09:10:43 +0000

    update: Dynamic Class name and dynamic title

--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,7 @@ router.get('/', (req, res, next) => {
   const products = adminData.products;
   // console.log('shop.js', adminData.products);
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-  res.render('shop', {prods: products, docTitle: 'Shop'});
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;

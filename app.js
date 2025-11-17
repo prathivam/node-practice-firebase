bcd520ca012c022d373ffa8cfe69b1b90f0ebbd2
sahrudhweb <sahrudhweb@gmail.com>
2025-11-17 05:53:02 +0000

    Setup Handlebars and move 404 page to handlebars

--- app.js
+++ app.js
@@ -2,24 +2,31 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const { engine } = require('express-handlebars');
 
 const app = express();
-app.set('view engine', 'pug')
-// If Views Folder was renamed to something else.
+
+app.engine(
+    'hbs',
+    engine({
+        extname: 'hbs',
+        defaultLayout: false,
+    })
+);
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
-
 const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
-app.use(shopRoutes);
+// app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'})
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000, () => {

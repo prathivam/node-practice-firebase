0627670c18d5f0da80d8e063e439d57febef2d66
sahrudhweb <sahrudhweb@gmail.com>
2025-11-17 06:38:03 +0000

    add: add-product.hbs file , shop.hbs file, main-layout.hbs

--- app.js
+++ app.js
@@ -9,21 +9,22 @@ const app = express();
 app.engine(
     'hbs',
     engine({
+        layoutsDir: 'views/layouts/',
+        defaultLayout: 'main-layout',
         extname: 'hbs',
-        defaultLayout: false,
     })
 );
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
-// app.use(shopRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
     res.status(404).render('404', { pageTitle: 'Page Not Found' });
--- routes/shop.js
+++ routes/shop.js
@@ -9,9 +9,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  // console.log('shop.js', adminData.products);
-  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0, activeShop: true, productCSS: true });
 });
 
 module.exports = router;

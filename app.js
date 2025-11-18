225f06051f250f790bef0959477e6eea6a7c0ce4
sahrudhweb <sahrudhweb@gmail.com>
2025-11-18 06:34:55 +0000

    update: Move 404 logic to controllers

--- app.js
+++ app.js
@@ -20,15 +20,15 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+const errors = require('./controllers/error');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
-});
+app.use(errors.get404);
 
 app.listen(3000, () => {
     console.log(`Server is running at http://localhost:3000`);

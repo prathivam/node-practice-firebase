5bc84788cf8bbfe11cec11b83063bcb8b9110960
sahrudhweb <sahrudhweb@gmail.com>
2025-11-18 08:50:21 +0000

    update: Move code into MVC model and data is now stored inside a local file

--- models/product.js
+++ models/product.js
@@ -1,15 +1,38 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(title){
-        this.title = title;
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    save(){
-        products.push(this);
-    }
+  save(cb) {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+        cb();
+      });
+    });
+  }
 
-    static fetchAll(){
-        return products;
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};

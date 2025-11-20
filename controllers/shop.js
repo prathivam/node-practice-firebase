3ab883c1099319c2f2a781327f6e4eef71dd093d
Prathi Sahrudh <prathi.sahrudh@valuemomentum.com>
2025-11-20 14:13:47 +0530

    update code

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,61 +1,62 @@
-const Product = require("../models/product");
-const path = require("../util/path");
+const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/product-list", {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
       prods: products,
-      pageTitle: "Products",
-      path: "/products",
-      hasProducts: products.length > 0,
-      // activeShop: true,
-      productCSS: true,
+      pageTitle: 'All Products',
+      path: '/products'
     });
   });
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, (product) => {
-    // console.log(p);
+  Product.findById(prodId, product => {
     res.render('shop/product-detail', {
-      pageTitle: product.title,
       product: product,
+      pageTitle: product.title,
       path: '/products'
-    })
+    });
   });
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/index", {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
       prods: products,
-      pageTitle: "Shop",
-      path: "/",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true,
+      pageTitle: 'Shop',
+      path: '/'
     });
   });
 };
 
 exports.getCart = (req, res, next) => {
-  res.render("shop/cart", {
-    pageTitle: "Cart",
-    path: "/cart",
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
   });
 };
 
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
+};
+
 exports.getOrders = (req, res, next) => {
-  res.render("shop/cart", {
-    pageTitle: "Cart",
-    path: "/cart",
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
   });
 };
 
 exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    pageTitle: "Checkout",
-    path: "/checkout",
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
   });
 };
--- routes/shop.js
+++ routes/shop.js
@@ -2,19 +2,22 @@ const path = require('path');
 
 const express = require('express');
 
-const shopController = require('../controllers/shop')
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
 router.get('/', shopController.getIndex);
+
 router.get('/products', shopController.getProducts);
-router.get('/products/delete');
+
 router.get('/products/:productId', shopController.getProduct);
 
-router.get('/cart', shopController.getCart)
-router.get('/orders', shopController.getOrders)
+router.get('/cart', shopController.getCart);
 
-router.get('/add-product', shopController.getProducts)
+router.post('/cart', shopController.postCart);
 
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;

7830e4e18fc57d764e99a88ae7dcba5b87e5e0c6
sahrudhweb <sahrudhweb@gmail.com>
2025-11-20 05:18:15 +0000

    add: Dynamic Routes - setup routes and controller

--- routes/shop.js
+++ routes/shop.js
@@ -7,12 +7,14 @@ const shopController = require('../controllers/shop')
 const router = express.Router();
 
 router.get('/', shopController.getIndex);
+router.get('/products', shopController.getProducts);
+router.get('/products/delete');
+router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart)
 router.get('/orders', shopController.getOrders)
 
 router.get('/add-product', shopController.getProducts)
 
-router.get('/products', shopController.getProducts);
 
 module.exports = router;
